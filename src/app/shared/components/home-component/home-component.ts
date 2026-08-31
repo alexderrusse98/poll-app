09a85df56cb84d96966f9e53e1db156afcd1f966
alexderrusse98 <alex.weiswasser.com@gmail.com>
2026-08-31 23:12:51 +0200

    add filter function in active & past

--- src/app/shared/components/home-component/home-component.ts
+++ src/app/shared/components/home-component/home-component.ts
@@ -10,16 +10,30 @@ import { SupabaseService } from '../../services/supabase';
 export class HomeComponent {
   surveysService = inject(SupabaseService)
 
-
   activeTab = signal<'active' | 'past'>('active')
 
-  
+  selectedCategory = signal<string>('all')
+
+  // availableCategories = computed(() =>
+  //[...new Set(this.surveysService.surveys().map(s => s.category))]
+  // )
+
+
+  availableCategories = computed(() => {
+    const allCategories = this.surveysService.surveys().map(s => s.category)
+    const unique = new Set(allCategories)
+    return [...unique]
+  })
+
+
   filteredSurveys = computed(() => {
     const now = new Date()
     if (this.activeTab() === 'active') {
       return this.surveysService.surveys().filter(s => new Date(s.end_date) > now)
+        .filter(s => this.selectedCategory() === 'all' || s.category === this.selectedCategory())
     } else {
       return this.surveysService.surveys().filter(s => new Date(s.end_date) < now)
+        .filter(s => this.selectedCategory() === 'all' || s.category === this.selectedCategory())
     }
   }
   )
